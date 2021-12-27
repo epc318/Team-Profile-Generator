d5bb64d39abe023d5dc18961e4e60771e737bf8a
Rick Johnson <carl4917@umn.edu>
2021-12-27 00:49:45 -0600

    2nd failing Employtee test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,3 +1,4 @@
+const { expect } = require("@jest/globals");
 const Employee = require("../lib/Employee");
 
 test("create an object for employee", () =>{
@@ -6,4 +7,10 @@ test("create an object for employee", () =>{
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 });
-
+test("get features for employee info", () =>{
+    const employee = new Employee("Rick", 3838, "rick@gmail.com");
+    expect(employee.getName()).toBe("James");
+    expect(employee.getId()).toEqual(expect.any(String));
+    expect(employee.getEmail()).toEqual(expect.any(Number));
+    expect(employee.getRole()).toBe("Manager");
+});    
