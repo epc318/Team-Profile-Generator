457bf91e7ec96391d3b119467e25b1a047e43e59
Rick Johnson <carl4917@umn.edu>
2021-12-27 01:39:32 -0600

    correcting engineer tests to pass

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -2,15 +2,15 @@ const Engineer = require("../lib/Engineer");
 
 test("create an object for Engineer", () =>{
     const engineer = new Engineer("Nelson", 1776, "nelson@gmail.com");
-    expect(engineer.name).toBe("John");
-    expect(engineer.id).toEqual(expect.any(String));
-    expect(engineer.email).toEqual(expect.any(Number));
+    expect(engineer.name).toBe("Nelson");
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toEqual(expect.any(String));
 });
 test("get engineers github username", () =>{
     const engineer = new Engineer("Nelson", 1776, "nelson@gmail.com");
-    expect(engineer.getGithub()).toEqual(expect.any(Number));
+    expect(engineer.getGithub()).toEqual(expect.any(String));
 });
 test("get engineers role (ie: Engineer)", () =>{
     const engineer = new Engineer("Nelson", 1776, "nelson@gmail.com");
-    expect(engineer.getRole()).toBe("Manager");
+    expect(engineer.getRole()).toBe("Engineer");
 });
