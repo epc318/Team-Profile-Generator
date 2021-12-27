e67f6250d218926c257fd018527f684d4c952792
Rick Johnson <carl4917@umn.edu>
2021-12-27 01:13:28 -0600

    fixing require link in Manager.js, fixing both initial mangers tests to pass

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -2,11 +2,11 @@ const Manager = require("../lib/Manager");
 
 test("create an object for Manager", () =>{
     const manager = new Manager("John", 1698, "john@gmail.com");
-    expect(manager.name).toBe("Rick");
-    expect(manager.id).toEqual(expect.any(String));
-    expect(manager.email).toEqual(expect.any(Number));
+    expect(manager.name).toBe("John");
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
 });
 test("get input for managers office number", () =>{
     const manager = new Manager("John", 1698, "john@gmail.com", 12);
-    expect(manager.id).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(Number));
 });    
