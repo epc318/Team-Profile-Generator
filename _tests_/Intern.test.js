999ce264292762d06f566455cf85149b645b1954
Rick Johnson <carl4917@umn.edu>
2021-12-27 01:59:42 -0600

    fixing camelcase error on intern.js getSchool, fixing get interns school test to test for the school AND getSchool. fixed all intern tests so they run successfully.

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,16 +1,18 @@
+const { expect } = require("@jest/globals");
 const Intern = require("../lib/Intern");
 
 test("create an object for Intern", () =>{
     const intern = new Intern("Kip", 9999, "kip@gmail.com");
-    expect(intern.name).toBe("Joe-Jack");
-    expect(intern.id).toEqual(expect.any(String));
-    expect(intern.email).toEqual(expect.any(Number));
+    expect(intern.name).toBe("Kip");
+    expect(intern.id).toEqual(expect.any(Number));
+    expect(intern.email).toEqual(expect.any(String));
 });
 test("get interns school", () =>{
-    const intern = new Intern("Kip", 9999, "kip@gmail.com");
-    expect(intern.getschool()).toEqual(expect.any(Number));
+    const intern = new Intern("Kip", 9999, "kip@gmail.com", "UCLA");
+    expect(intern.school).toEqual(expect.any(String));
+    expect(intern.getSchool()).toBe(intern.school);
 });
 test("get interns role (ie: Intern)", () =>{
     const intern = new Intern("Kip", 9999, "kip@gmail.com");
-    expect(intern.getRole()).toBe("Manager");
+    expect(intern.getRole()).toBe("Intern");
 });
