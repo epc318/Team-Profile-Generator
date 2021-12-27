d185d3d5d60d2e78fa1dce5bff857962950bf4e1
Rick Johnson <carl4917@umn.edu>
2021-12-27 00:45:07 -0600

    wrote first passing Employee test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,8 +1,9 @@
-const Employee = require("./lib/Employee.js");
+const Employee = require("../lib/Employee");
 
 test("create an object for employee", () =>{
-    const employee = new Employee("Rick", 3838, rick@gmail.com);
-    expect(employee.name).toBe("James");
-    expect(employee.id).toEqual(expect.any(String));
-    expect(employee.email).toEqual(expect.any(Number));
+    const employee = new Employee("Rick", 3838, "rick@gmail.com");
+    expect(employee.name).toBe("Rick");
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
 });
+
