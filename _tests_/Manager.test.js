4f6022b17c4f6d855f426def60b2906fbea080b3
Rick Johnson <carl4917@umn.edu>
2021-12-27 01:08:58 -0600

    creating first two failing tests for Manager

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,1 +1,12 @@
 const Manager = require("../lib/Manager");
+
+test("create an object for Manager", () =>{
+    const manager = new Manager("John", 1698, "john@gmail.com");
+    expect(manager.name).toBe("Rick");
+    expect(manager.id).toEqual(expect.any(String));
+    expect(manager.email).toEqual(expect.any(Number));
+});
+test("get input for managers office number", () =>{
+    const manager = new Manager("John", 1698, "john@gmail.com", 12);
+    expect(manager.id).toEqual(expect.any(String));
+});    
