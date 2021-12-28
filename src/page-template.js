595067604b2107596db45f659744626e09c814c0
Rick Johnson <carl4917@umn.edu>
2021-12-27 22:51:34 -0600

    creating function for manager html template

--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,38 @@
+function generateManager(manager) {
+const htmlManagerTemp = manager.map(({ name, id, email, officeNumber }) => {
+    return `
+    <div>
+        <div>
+            <h1> Team Manager</h1>
+        </div>
+        <div>
+            <div>
+                <h1> ${name}</h1>
+            </div>
+            <div>
+                 <h3> Employee ID #: ${id}</h3>
+            </div>
+            <div>
+                <h3>Email Address: ${email}</a></h3>
+            </div>
+            <div>
+                <h3>Office Number: ${officeNumber}</h3>
+            </div>
+        </div>
+    </div>`;
+    });
+
+    return `${managerHTML.join('')}`;
+};
+
+
+
+
+
+
+
+
+
 function generatePage(data) {
     return `
     <!DOCTYPE html>
@@ -9,7 +44,9 @@ function generatePage(data) {
             <title>Team Profile Generator</title>
             <link rel="stylesheet" href="style.css">
         </head>
+
         <body>
+
             <header></header>
 
 
