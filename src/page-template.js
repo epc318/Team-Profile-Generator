611c1a38258673c6654436412cde9ca433690a67
Rick Johnson <carl4917@umn.edu>
2021-12-27 23:00:00 -0600

    adding engineer and intern html templates.

--- src/page-template.js
+++ src/page-template.js
@@ -3,33 +3,79 @@ const htmlManagerTemp = manager.map(({ name, id, email, officeNumber }) => {
     return `
     <div>
         <div>
-            <h1> Team Manager</h1>
+            <h1>Team Manager</h1>
         </div>
         <div>
             <div>
-                <h1> ${name}</h1>
+                <h1>${name}</h1>
             </div>
             <div>
-                 <h3> Employee ID #: ${id}</h3>
+                 <h3>Employee ID #:${id}</h3>
             </div>
             <div>
-                <h3>Email Address: ${email}</a></h3>
+                <h3>Email Address:${email}</a></h3>
             </div>
             <div>
-                <h3>Office Number: ${officeNumber}</h3>
+                <h3>Office Number:${officeNumber}</h3>
             </div>
         </div>
     </div>`;
     });
-
-    return `${managerHTML.join('')}`;
+    return `
+    ${managerHTML.join('')}`;
 };
-
-
-
-
-
-
+function generateEngineer(engineer) {
+    const htmlEngineerTemp = engineer.map(({ name, id, email, github }) => {
+        return `
+        <div>
+            <div>
+                <h1>Engineer</h1>
+            </div>
+            <div>
+                <div>
+                    <h1>${name}</h1>
+                </div>
+                <div>
+                     <h3>Employee ID #:${id}</h3>
+                </div>
+                <div>
+                    <h3>Email Address:${email}</a></h3>
+                </div>
+                <div>
+                    <h3>Github Username:${github}</h3>
+                </div>
+            </div>
+        </div>`;
+        });
+        return `
+        ${engineerHTML.join('')}`;
+    };
+function generateIntern(intern) {
+    const htmlInternTemp = intern.map(({ name, id, email, school }) => {
+        return `
+        <div>
+            <div>
+                <h1>Intern</h1>
+            </div>
+            <div>
+                <div>
+                    <h1>${name}</h1>
+                </div>
+                <div>
+                     <h3>Employee ID #:${id}</h3>
+                </div>
+                <div>
+                    <h3>Email Address:${email}</a></h3>
+                </div>
+                <div>
+                    <h3>School/University:${school}</h3>
+                </div>
+            </div>
+        </div>`;
+        });
+        return `
+        ${internHTML.join('')}`;
+    };
 
 
 
