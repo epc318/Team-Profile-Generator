844bd27e7e6ac2c1833e45695c5edd4bb0020472
Rick Johnson <carl4917@umn.edu>
2021-12-27 22:23:07 -0600

    finished them statements. tentatively finsihed with index.js

--- index.js
+++ index.js
@@ -231,3 +231,19 @@ Add an Intern
 promptManager({})
 .then(promptEngineer)
 .then(promptIntern)
+.then(employeeData => {
+    return generatePage(employeeData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
