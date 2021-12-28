3d175a558d25360d14f575c8419c9482f4afb847
Rick Johnson <carl4917@umn.edu>
2021-12-27 21:34:26 -0600

    finished manager prompts, moving on to Engineer prompts

--- index.js
+++ index.js
@@ -5,15 +5,93 @@ const Intern = require('./lib/Intern');
 const Engineeer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 
-const promptManager = employeeData => {
-
-    console.log(`
-  ===========
-  New Project
-  ===========
-  `);
-     if(!employeeData.manager) {
-         employeeData.manager = []
-     }
+const promptTeamManager = employeeData => {
+     if(!employeeData.teamManager) {
+         employeeData.teamManager = []
+     };
+     console.log(`
+     =================
+     Add a New Manager
+     =================
+   `);
      return inquirer.prompt([
         {
+          type: 'input',
+          name: 'name',
+          message: 'What is the team Managers name?',
+          validate: nameInput => {
+              if (nameInput) {
+              return true;
+              } else {
+              console.log('Please enter the team Managers name!');
+              return false;
+              }
+            }
+        
+        },
+        {
+            type: 'number',
+            name: 'id',
+            message: 'What is the team Managers ID #?',
+            validate: nameInput => {
+                if (nameInput) {
+                return true;
+                } else {
+                console.log('Please enter the team Managers ID number!');
+                return false;
+                }
+              },
+          
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the team Managers email address?',
+            validate: nameInput => {
+                if (nameInput) {
+                return true;
+                } else {
+                console.log('Please enter the team Managers full email address!');
+                return false;
+                }
+              },
+        },
+        {
+            type: 'number',
+            name: 'officeNum',
+            message: 'What is the team Managers Office Number?',
+            validate: nameInput => {
+                if (nameInput) {
+                return true;
+                } else {
+                console.log('Please enter the team Managers Office Number!');
+                return false;
+                }
+              },
+        },
+    ])
+    .then(({ name, id, email, officeNumber, confirmAddTeamManager }) => {
+        employeeData.teamManager.push(new Manager(name, id, email, officeNumber));
+            if(confirmAddTeamManager) {
+                return promptTeamManager(employeeData);
+            }
+
+            return employeeData;
+    })
+};
+const promptEngineer = employeeData => {
+    if(!employeeData.engineer) {
+        employeeData.engineer = []
+    };
+    console.log(`
+    ===============
+    Add an Engineer
+    ===============
+  `);
+
+
+
+
+
+  
+}         
