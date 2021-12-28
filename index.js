6f28292e8e66a9cf46333f1c0e3c458d9202928b
Rick Johnson <carl4917@umn.edu>
2021-12-27 21:59:58 -0600

    adding intern prompts, beginning end application then statements

--- index.js
+++ index.js
@@ -152,12 +152,82 @@ Add an Engineer
     
                 return employeeData;
         })
+};
+const promptIntern = employeeData => {
+    if(!employeeData.intern) {
+        employeeData.intern = []
     };
-  
+    console.log(`
+===============
+Add an Intern
+===============
+    `);
+        return inquirer.prompt([
+            {
+            type: 'input',
+            name: 'name',
+            message: 'What is the Interns name?',
+            validate: nameInput => {
+                if (nameInput) {
+                return true;
+                } else {
+                console.log('Please enter the Interns name!');
+                return false;
+                }
+                }
+            
+            },
+            {
+                type: 'number',
+                name: 'id',
+                message: 'What is the team Interns ID #?',
+                validate: nameInput => {
+                    if (nameInput) {
+                    return true;
+                    } else {
+                    console.log('Please enter the Interns ID number!');
+                    return false;
+                    }
+                },
+            
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the Interns email address?',
+                validate: nameInput => {
+                    if (nameInput) {
+                    return true;
+                    } else {
+                    console.log('Please enter the Interns full email address!');
+                    return false;
+                    }
+                },
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'What is the Interns most recent or current school?',
+                validate: nameInput => {
+                    if (nameInput) {
+                    return true;
+                    } else {
+                    console.log('Please enter the Interns most recent or current school!');
+                    return false;
+                    }
+                },
+            },
+        ])
+        .then(({ name, id, email, school, confirmAddIntern }) => {
+            employeeData.intern.push(new Intern(name, id, email, school));
+                if(confirmAddIntern) {
+                    return promptIntern(employeeData);
+                }
+    
+                return employeeData;
+        })
+};
 
-
-
-
-
-
-}         
+promptManager({})
+.then(promptEngineer)
+.then(promptIntern)
