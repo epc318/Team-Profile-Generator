7aa9e61e99fa8758da56947af6b756543da9e0eb
Rick Johnson <carl4917@umn.edu>
2021-12-27 20:52:49 -0600

    finished generateHTML.js

--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -14,3 +14,19 @@ const writeFile = fileContent => {
       });
     });
   };
+  const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+              reject(err);
+              return;
+            }
+        resolve({
+          ok: true,
+          message: 'stylesheet Successfully created!'
+        });
+      });
+    });
+  };
+
+  module.exports = { writeFile, copyFile };
