14317ced2d19a9095e46955c7ef21dd627b1206b
Rick Johnson <carl4917@umn.edu>
2021-12-27 22:36:12 -0600

    adding skeleton for the generatePage functions html data

---
+++ src/page-template.js
@@ -0,0 +1,23 @@
+function generatePage(data) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Team Profile Generator</title>
+            <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
+            <header></header>
+
+
+            <main></main>
+
+
+            <footer></footer>
+        </body>
+    </html>
+        `
+}
